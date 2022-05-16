b54eb2a8cf379bffe123caa6001ed7bd7f2bdcad
camiloJosue222 <camilojosue222@gmail.com>
2022-05-16 03:15:17 -0500

    Agregar archivos de proyecto.

---
+++ MVCCB/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using MVCCB.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<TelaViewModel> lst = null;
+            using (Models.BD_TALLERDECONFECCION2Entities db = new Models.BD_TALLERDECONFECCION2Entities())
+            {
+                 lst =
+                    (from d in db.tb_tela
+                     select new TelaViewModel
+                     {
+                         Id = (int)d.tela_id,
+                         Nombre = d.tela_nom
+                     }).ToList();
+            }
+            List<SelectListItem> items = lst.ConvertAll(d =>
+            {
+                return new SelectListItem()
+                {
+                    Text = d.Nombre.ToString(),
+                    Value = d.Id.ToString(),
+                    Selected = false
+                };
+            });
+            ViewBag.items = items;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVCCB/Models/ViewModel/TelaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCCB.Models.ViewModel
+{
+    public class TelaViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
